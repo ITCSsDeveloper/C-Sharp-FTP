78bcdff9be81792d8cf002c525e057234fd1700c
ITCS'sDeveloper <itcss.developer.me@gmail.com>
2017-05-11 10:13:50 +0700

    Upload Example Code C# WinSCP.NET

--- C-Sharp-FTP/C-Sharp-FTP/Program.cs
+++ C-Sharp-FTP/C-Sharp-FTP/Program.cs
@@ -1,15 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using WinSCP;
 
 namespace C_Sharp_FTP
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            // FTP whith WinSCP .NET
+            // Ref : https://winscp.net/eng/docs/library
+
+            try
+            {
+                // Setup session options
+                SessionOptions sessionOptions = new SessionOptions
+                {
+                    Protocol = Protocol.Sftp,
+                    HostName = "example.com",
+                    UserName = "user",
+                    Password = "mypassword",
+                    FtpMode = FtpMode.Passive,
+                    FtpSecure = FtpSecure.Explicit,
+                    SshHostKeyFingerprint = "ssh-rsa 2048 xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx",
+                };
+
+                using (Session session = new Session())
+                {
+                    // Connect
+                    session.Open(sessionOptions);
+
+                    // Upload files
+                    TransferOptions transferOptions = new TransferOptions();
+                    transferOptions.TransferMode = TransferMode.Binary;
+
+                    TransferOperationResult transferResult;
+                    transferResult = session.PutFiles(@"d:\toupload\*", "/home/user/", false, transferOptions);
+
+                    // Throw on any error
+                    transferResult.Check();
+
+                    // Print results
+                    foreach (TransferEventArgs transfer in transferResult.Transfers)
+                    {
+                        Console.WriteLine("Upload of {0} succeeded", transfer.FileName);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e);
+            }
         }
     }
 }
